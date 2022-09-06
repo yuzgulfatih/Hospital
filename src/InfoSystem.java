c54ef9e3b965e2bfe6ae3ee26d37a449e4a4fdf3
muhammedfth1 <92013695+muhammedfth1@users.noreply.github.com>
2022-09-07 00:45:41 +0300

    project comlepeted

--- src/InfoSystem.java
+++ src/InfoSystem.java
@@ -3,14 +3,68 @@ import java.util.List;
 
 public class InfoSystem {
     static List<Doctor> doctors = new ArrayList<>();
-    static List<Patient> patients = new ArrayList<>();
-    boolean isTrue;
+    static List<Patient> patients = new ArrayList<Patient>();
+    boolean isPatient;
     boolean isDoctor;
     boolean isTherapy;
 
-    boolean addPatient(Patient patient){
+    boolean addPatient(Patient patient) {
         patients.add(patient);
         System.out.println("Added patient ID number " + patient.id);
         return true;
+    }
+
+    Patient findPatient(String id) throws PersonNotFoundException {
+        isPatient = false;
+        for (int i = 0; i < patients.size(); i++) {
+            if (id == patients.get(i).id) {
+                isPatient = true;
+            }
+            if (isPatient) {
+                System.out.println("There is a patient with ID number  " + id);
+            } else {
+                throw new PersonNotFoundException("No patient with ID number " + id);
+            }
+        }
+        return null;
+    }
+
+    boolean addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+        System.out.println("Added doctor ID number " + doctor.id);
+        return true;
+    }
+
+    Doctor findDoctor(String id) throws PersonNotFoundException {
+        isDoctor = false;
+        for (int j = 0; j < doctors.size(); j++) {
+            if (id == doctors.get(j).id) {
+                isDoctor = true;
+            }
+            if (isDoctor) {
+                System.out.println("There is a doctor with ID number  " + id);
+            } else {
+                throw new PersonNotFoundException("No doctor with ID number " + id);
+            }
+        }
+        return null;
+    }
+
+    Therapy addTherapy(Patient patient , Doctor doctor) throws PersonNotFoundException{
+        isTherapy = false;
+        for (int k = 0; k < patients.size(); k++) {
+            for (int l = 0; l < doctors.size(); l++) {
+                if (patient == patients.get(k) && doctor == doctors.get(l)){
+                    isTherapy = true;
+                }
+            }
+        }
+
+        if (isTherapy){
+            System.out.println("Therapy created");
+        }else {
+            throw new PersonNotFoundException("Therapy not created");
+        }
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PersonNotFoundException {
         InfoSystem infoSystem = new InfoSystem();
         Patient patient = new Patient("Fatih" , "121312" , 2002);
+        Doctor doctor = new Doctor("Emine" , "3432432" , 25);
+        infoSystem.addDoctor(doctor);
         infoSystem.addPatient(patient);
+        infoSystem.addTherapy(patient , doctor);
     }
 }
